37330904e97b8da6b67988afeff6aea7f2ca9a47
arleilps <arleilps@9cb20d3c-15ea-c663-d894-0ba4c12ea272>
2012-06-27 17:00:10 +0000

    The bash script now applies different parameters depending on the database.

--- trunk/experiment_content_recommendation/run_experiment.sh
+++ trunk/experiment_content_recommendation/run_experiment.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 databases_dir='../../data/'
-#databases=("fiat" "brasileirao" "elections") 
-databases=("fiat") 
+databases=("fiat" "brasileirao" "elections") 
+#databases=("fiat") 
 train_rate=0.5
-min_freq_content=2
-min_freq_user=5
+min_freq_content=("2" "5" "5")
+min_freq_user=("5" "10" "10")
 num_iterations_profilerank=10
 damping_factor=0.85
 my_media_lite_methods=("BPRMF" "ItemKNN" "Random" "UserKNN" "WRMF" "MostPopular" "WeightedItemKNN" "WeightedUserKNN" "Zero" "SoftMarginRankingMF" "WeightedBPRMF")
+
+d=0
 
 for database in ${databases[@]}
 do
 	echo "database: $database"
 	
 	echo "Generating data"
-	python generate_data.py -t train-$database.csv -e test-$database.csv -r $train_rate -c $min_freq_content -u $min_freq_user $databases_dir$database\_tweets.csv -s
+	python generate_data.py -t train-$database.csv -e test-$database.csv -r $train_rate -c ${min_freq_content[$d]} -u ${min_freq_user[$d]} $databases_dir$database\_tweets.csv -s
 	
 	echo "Running methods"
 	python run.py -n $num_iterations_profilerank -d $damping_factor -o $database train-$database.csv test-$database.csv -s
@@ -28,5 +30,7 @@ do
 		echo "Evaluating $method"
 		python evaluate.py -o $database-$method $database\_$method.csv test-$database.csv -s
 	done
-	
+        
+	d=$(($d+1))
 done
+
